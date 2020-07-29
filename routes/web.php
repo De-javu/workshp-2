8d29ea52bddac6995d1262b1e2d38b32252050ac
andres pardo <andrespardo5151@gmail.com>
2020-07-29 11:33:39 -0500

    validacion de correo electronico

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,9 @@ Route::get('/', function () {
 
 
 
-Route::get('/home', 'HomeController@index')->name('home');
+
 
 Auth::routes();
+Auth::routes(['verify' => true]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home')->middleware('verified');
